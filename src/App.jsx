4a3ca40a72e74dc6f13304d4844ddad2c73f2e02
GerGG <germain.gg90@gmail.com>
2024-03-12 17:35:52 -0400

    func nav created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import HomePage from "./components/HomePage";
+import About from "./components/About";
+
+const NAVIGATION_EVENT = "pushstate";
+
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        {currentPath === "/" && <HomePage />}
+        {currentPath === "/about" && <About />}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <a href="/about">About</a>
+    </>
+  );
+}
+
+export default HomePage;
